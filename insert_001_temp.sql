97fe0f1dc3f598100ba9e8aef7d95ba2adeb37bc
maixuandiep810 <maixuandiep810@gmail.com>
2020-11-30 14:35:25 +0700

    30112020

--- insert_001_temp.sql
+++ insert_001_temp.sql
@@ -16,13 +16,13 @@ VALUES
 ;
 
 INSERT INTO `Categories` VALUES 
-(41,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','appearance','appearance, body, clothes and fashion, colours and shapes','/category/image/appearance_1.png', 1, 0, NULL),
-(42,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','animals','animals, birds, fish, shellfish','/category/image/animals_1.png', 1, 0, NULL),
-(43,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','communication','language, phones, email,the internet','/category/image/communication_1.png', 1, 0, NULL),
-(44,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','culture','art, film, literature, music','/category/image/culture_1.png', 1, 0, NULL),
-(45,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','food and drink','cooking, eating, drinks, food','/category/image/food-and-drink_1.png', 1, 0, NULL),
-(46,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','health','health, fitmess, illness, medicine','/category/image/health_1.png', 1, 0, NULL),
-(47,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','homes and buildings','buidings, gardens, houses, homes','/category/image/homes-and-buildings_1.png', 1, 0, NULL)
+(41,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','appearance','appearance, body, clothes and fashion, colours and shapes','/category/image/appearance_1.png', 1),
+(42,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','animals','animals, birds, fish, shellfish','/category/image/animals_1.png', 1),
+(43,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','communication','language, phones, email,the internet','/category/image/communication_1.png', 1),
+(44,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','culture','art, film, literature, music','/category/image/culture_1.png', 1),
+(45,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','food and drink','cooking, eating, drinks, food','/category/image/food-and-drink_1.png', 1),
+(46,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','health','health, fitmess, illness, medicine','/category/image/health_1.png', 1),
+(47,1,'2020-10-29 10:02:49','2020-10-29 10:02:49','homes and buildings','buidings, gardens, houses, homes','/category/image/homes-and-buildings_1.png', 1)
 ;
 
 INSERT INTO `Vocabularies` VALUES 
@@ -50,29 +50,13 @@ VALUES
 (6,1,'2020-10-29 11:04:59','2020-10-29 11:04:59',1,"ToDoTestC2","10","OK")
 ;
 
-
-
-
-
-
-
-
-
-
-
-
-
--- INSERT INTO `Vocabularies` VALUES 
--- (3,1,'2020-10-29 11:04:59','2020-10-29 11:04:59','beautiful','','','','','/vocabulary/image/appearance_1.png',41),
--- (9,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','blonde','','','','','/vocabulary/image/appearance_1.png',41),
--- (10,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','dark','','','','','/vocabulary/image/appearance_1.png',41),
--- (11,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','fat','','','','','/vocabulary/image/appearance_1.png',41),
--- (12,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','gray','','','','','/vocabulary/image/appearance_1.png',41),
--- (13,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','grey','','','','','/vocabulary/image/appearance_1.png',41),
--- (14,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','hair','','','','','/vocabulary/image/appearance_1.png',41),
--- (15,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','long','','','','','/vocabulary/image/appearance_1.png',41),
--- (16,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','pretty','','','','','/vocabulary/image/appearance_1.png',41),
--- (17,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','red','','','','','/vocabulary/image/appearance_1.png',41),
--- (18,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','short','','','','','/vocabulary/image/appearance_1.png',41),
--- (19,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','tall','','','','','/vocabulary/image/appearance_1.png',41)
--- ;
+INSERT INTO `vocab4`.`UserCategories`
+VALUES
+(1,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 41, 0, 0),
+(2,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 42, 0, 1),
+(3,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 43, 0, 1),
+(4,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 44, 0, 1),
+(5,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 45, 0, 0),
+(6,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 46, 0, 0),
+(7,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 47, 0, 0)
+;
