208f7170bff04ba0022efa450f23f3a35403c952
maixuandiep810 <maixuandiep810@gmail.com>
2021-01-14 10:01:52 +0700

    aaaa

--- insert_001_temp.sql
+++ insert_001_temp.sql
@@ -26,10 +26,10 @@ INSERT INTO `Categories` VALUES
 ;
 
 INSERT INTO `Vocabularies` VALUES 
-(101,1,'2020-10-29 11:04:59','2020-10-29 11:04:59','beautiful','','having beauty; giving pleasure to the senses or to the mind','ˈbjuːtɪfl','','/vocabulary/image/beautiful.png',41),
-(102,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','blonde','','(of hair) pale gold in colour','blɒnd','','/vocabulary/image/blonde.jpeg',41),
-(103,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','fat','','(of a person’s or an animal’s body) having too much flesh on it and weighing too much','fæt','','/vocabulary/image/fat.jpeg',41),
-(104,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','tall','','(of a person, building, tree, etc.) having a greater than average height','tɔːl','','/vocabulary/image/tall.jpeg',41)
+(1,1,'2020-10-29 11:04:59','2020-10-29 11:04:59','beautiful','','having beauty; giving pleasure to the senses or to the mind','ˈbjuːtɪfl','','/vocabulary/image/beautiful.png',41),
+(2,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','blonde','','(of hair) pale gold in colour','blɒnd','','/vocabulary/image/blonde.jpeg',41),
+(3,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','fat','','(of a person’s or an animal’s body) having too much flesh on it and weighing too much','fæt','','/vocabulary/image/fat.jpeg',41),
+(4,1,'2020-10-29 11:07:25','2020-10-29 11:07:25','tall','','(of a person, building, tree, etc.) having a greater than average height','tɔːl','','/vocabulary/image/tall.jpeg',41)
 ;
 
 INSERT INTO `Questions` VALUES 
@@ -50,7 +50,7 @@ VALUES
 (6,1,'2020-10-29 11:04:59','2020-10-29 11:04:59',1,"ToDoTestC2","10","OK")
 ;
 
-INSERT INTO `vocab4`.`UserCategories`
+INSERT INTO `UserCategories`
 VALUES
 (1,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 41, 0, 0),
 (2,1,'2020-10-29 11:04:59','2020-10-29 11:04:59', 1, 42, 0, 1),
